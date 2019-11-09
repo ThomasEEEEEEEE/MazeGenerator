96daf19240dd85f380070502ae92d46a837cf662
Thomas Eberhart <thomas.eberhart@oit.edu>
2019-11-09 11:06:11 -0800

    Maze works!!!!11!!!1!1 YEET

--- MazeBuilder/MazeBuilder/MazeBuilder.cpp
+++ MazeBuilder/MazeBuilder/MazeBuilder.cpp
@@ -23,8 +23,14 @@ struct Cell
 	bool Visited = false;
 };
 
-const int MazeLen = 70;
-const int MazeWid = 40;
+struct Coords
+{
+	int x = 0;
+	int y = 0;
+};
+
+const int MazeLen = 80;
+const int MazeWid = 45;
 
 class MazeBuilder : public PixelGameEngine
 {
@@ -35,136 +41,145 @@ public:
 	}
 
 	
-	stack<Cell> Stack;
+	stack<Coords> Stack;
 	Cell cells[MazeLen][MazeWid];
 	int VisitedCells;
 	int TileSize;
-	int CurrentX;
-	int CurrentY;
 
 	bool OnUserCreate() override
 	{
 		srand(time(0));
-		VisitedCells = 0;
-		TileSize = 2;
-		Cell Start;
-		Stack.push(Start);
-		CurrentX = 0;
-		CurrentY = 0;
+		VisitedCells = 1;
+		TileSize = 3;
+		Coords c;
+		c.x = 0;
+		c.y = 0;
+		Stack.push(c);
+		cells[0][0].Visited = true;
 
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		vector<Dir> temp;
-		temp.clear();
-		try
+		//this_thread::sleep_for(10ms);
+		Clear(BLACK);
+
+		if (VisitedCells < MazeLen * MazeWid)
 		{
-			if (cells[CurrentX][CurrentY - 1].Visited == false)
+			vector<Dir> temp;
+			temp.clear();
+
+
+			if (Stack.top().y > 0 && cells[Stack.top().x][Stack.top().y - 1].Visited == false)
 			{
 				temp.push_back(Dir::N);
 			}
-		}
-		catch (exception) {}
 
-		try
-		{
-			if (cells[CurrentX + 1][CurrentY].Visited == false)
+			if (Stack.top().x < MazeLen - 1 && cells[Stack.top().x + 1][Stack.top().y].Visited == false)
 			{
 				temp.push_back(Dir::E);
 			}
-		}
-		catch (exception) {}
 
-		try
-		{
-			if (cells[CurrentX][CurrentY + 1].Visited == false)
+			if (Stack.top().y < MazeWid - 1 && cells[Stack.top().x][Stack.top().y + 1].Visited == false)
 			{
 				temp.push_back(Dir::S);
 			}
-		}
-		catch (exception) {}
 
-		try
-		{
-			if (cells[CurrentX - 1][CurrentY].Visited == false)
+			if (Stack.top().x > 0 && cells[Stack.top().x - 1][Stack.top().y].Visited == false)
 			{
 				temp.push_back(Dir::W);
 			}
-		}
-		catch (exception) {}
 
 
-		cells[CurrentX][CurrentY].Visited = true;
 
-		if (temp.empty())
-		{
-			Stack.pop();
+			if (temp.empty())
+			{
+				Stack.pop();
+			}
+			else
+			{
+				int dir = rand() % temp.size();
+				Dir d = temp[dir];
+				Coords c;
+
+				switch (d)
+				{
+				case Dir::N:
+					c.x = Stack.top().x;
+					c.y = Stack.top().y - 1;
+					cells[Stack.top().x][Stack.top().y].N = true;
+					Stack.push(c);
+					cells[Stack.top().x][Stack.top().y].S = true;
+					break;
+				case Dir::E:
+					c.x = Stack.top().x + 1;
+					c.y = Stack.top().y;
+					cells[Stack.top().x][Stack.top().y].E = true;
+					Stack.push(c);
+					cells[Stack.top().x][Stack.top().y].W = true;
+					break;
+				case Dir::S:
+					c.x = Stack.top().x;
+					c.y = Stack.top().y + 1;
+					cells[Stack.top().x][Stack.top().y].S = true;
+					Stack.push(c);
+					cells[Stack.top().x][Stack.top().y].N = true;
+					break;
+				case Dir::W:
+					c.x = Stack.top().x - 1;
+					c.y = Stack.top().y;
+					cells[Stack.top().x][Stack.top().y].W = true;
+					Stack.push(c);
+					cells[Stack.top().x][Stack.top().y].E = true;
+					break;
+				}
+				cells[Stack.top().x][Stack.top().y].Visited = true;
+				++VisitedCells;
+			}
 		}
 		else
 		{
-			int dir = rand() % temp.size();
-			Dir d = temp[dir];
-
-			switch (d)
-			{
-			case Dir::N:
-				Stack.push(cells[CurrentX][CurrentY + 1]);
-				cells[CurrentX][CurrentY].N = true;
-				++CurrentY;
-				break;
-			case Dir::E:
-				Stack.push(cells[CurrentX + 1][CurrentY]);
-				cells[CurrentX][CurrentY].E = true;
-				++CurrentX;
-				break;
-			case Dir::S:
-				Stack.push(cells[CurrentX][CurrentY - 1]);
-				cells[CurrentX][CurrentY].S = true;
-				--CurrentY;
-				break;
-			case Dir::W:
-				Stack.push(cells[CurrentX - 1][CurrentY]);
-				cells[CurrentX][CurrentY].W = true;
-				--CurrentX;
-				break;
-			}
+			//end whole program
+			char ch;
+			cin >> ch;
+			return false;
 		}
+
 
 
 		for (int x = 0; x < MazeLen; ++x)
 		{
 			for (int y = 0; y < MazeWid; ++y)
 			{
-				if (cells[x][y].Visited)
+				if (Stack.top().x == x && Stack.top().y == y)
 				{
-					FillRect(x * 2 * TileSize, y * 2 * TileSize, 2 * TileSize, 2 * TileSize, CYAN);
+					FillRect(Stack.top().x * (TileSize + 1), Stack.top().y * (TileSize + 1), TileSize, TileSize, GREEN);
 				}
 				else
 				{
-					FillRect(x * 2 * TileSize, y * 2 * TileSize, 2 * TileSize, 2 * TileSize, WHITE);
-				}
+					if (cells[x][y].Visited)
+					{
+						FillRect(x * (TileSize + 1), y * (TileSize + 1), TileSize, TileSize, WHITE);
+					}
+					else
+					{
+						FillRect(x * (TileSize + 1), y * (TileSize + 1), TileSize, TileSize, BLUE);
+					}
 
-				if (cells[x][y].E)
-				{
-					FillRect(x * 2 * TileSize + 1, y * 2 * TileSize, TileSize, TileSize * 2, CYAN);
-				}
-				else
-				{
-					FillRect(x * 2 * TileSize + 1, y * 2 * TileSize, TileSize, TileSize * 2, BLACK);
-
-				}
-				if (cells[x][y].S)
-				{
-					FillRect(x * 2 * TileSize, y * 2 * TileSize + 1, TileSize * 2, TileSize, CYAN);
-				}
-				else
-				{
-					FillRect(x * 2 * TileSize, y * 2 * TileSize + 1, TileSize * 2, TileSize, BLACK);
+					if (cells[x][y].S == true)
+					{
+						FillRect(x * (TileSize + 1), y * (TileSize + 1) + TileSize, 3, 1, WHITE);
+					}
+					if (cells[x][y].E == true)
+					{
+						FillRect(x * (TileSize + 1) + TileSize, y * (TileSize + 1), 1, 3, WHITE);
+					}
 				}
 			}
 		}
+
+		
 		return true;
 	}
 };
